ff8e744756295b29c435551c42083a84548cae5b
sitoff <svlsitoff@gmail.com>
2022-01-23 21:49:35 +0800

    Дела идут контора пишет

---
+++ core/src/com/mygdx/game/models/FPick.java
@@ -0,0 +1,10 @@
+package com.mygdx.game.models;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class FPick extends GameObject {
+
+    public FPick(Texture texture, float x, float y, float width, float height) {
+        super(texture,x,y,width,height);
+    }
+}
